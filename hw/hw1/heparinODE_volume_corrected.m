583aae6bc1c0443559cdad86f327b34b4d5a5352
Vivek Booshan <vivek.booshan@gmail.com>
2024-02-10 20:42:59 -0500

    add q2f plot and clean up code and rename m4c_eqns to heparinODE and incorporate volumecorrection

--- hw/hw1/heparinODE_volume_corrected.m
+++ hw/hw1/heparinODE_volume_corrected.m
@@ -1,4 +1,4 @@
-function dydt = m4C_eqns_Heparin(~, y, p)
+function dydt = heparinODE_volume_corrected(t, y, p)
     
     % y1 = D_BP : blood plasma drug
     % y2 = DP : drug-protein complex
@@ -7,26 +7,27 @@ function dydt = m4C_eqns_Heparin(~, y, p)
     % y5 = D_b : drug in body
     % y6 = D_c : drug cleared
     % y7 = P : protein
-
+    
     % kon : rate constant of plasma protein binding
     % koff : rate constant of plasma protein unbinding
     % kt1 & kt1r : rate constant of tumor 1 and reverse direction
     % kt2 & kt2r : rate constant of tumor 2 and reverse direction
     % kb & kbr : rate constant of body and reverse
     % kc : rate constant of clearance
-
+    
     dydt = zeros(6, 1);
-
+    
     dydt(1) = ( ...
         - p.kon * p.vbp * y(1) + p.koff * p.vbp * y(2) ...
-        - p.kt1 * p.vbp * y(1) + p.kt1r * p.vt1 * y(3) ...
-        - p.kt2 * p.vbp * y(1) + p.kt2r * p.vt2 * y(4) ...
+        - p.kt1 * (p.vt1/p.vbp) * p.vbp * y(1) + p.kt1r * p.vt1 * y(3) ...
+        - p.kt2 * (p.vt2/p.vbp) * p.vbp * y(1) + p.kt2r * p.vt2 * y(4) ...
         - p.kb  * p.vbp * y(1) + p.kbr  * p.vb  * y(5) ...
         - p.kc  * p.vbp * y(1) ...
     ) / p.vbp;
     dydt(2) = (p.kon * p.vbp * y(1) - p.koff * p.vbp  * y(2)) / p.vbp;
-    dydt(3) = (p.kt1 * p.vbp * y(1) - p.kt1r * p.vt1  * y(3)) / p.vt1;
-    dydt(4) = (p.kt2 * p.vbp * y(1) - p.kt2r * p.vt2  * y(4)) / p.vt2;
+    dydt(3) = (p.kt1 * (p.vt1/p.vbp) * p.vbp * y(1) - p.kt1r * p.vt1  * y(3)) / p.vt1;
+    dydt(4) = (p.kt2 * (p.vt2/p.vbp) * p.vbp * y(1) - p.kt2r * p.vt2  * y(4)) / p.vt2;
     dydt(5) = (p.kb  * p.vbp * y(1) - p.kbr  * p.vb   * y(5)) / p.vb;
     dydt(6) =  p.kc  * p.vbp * y(1);
     dydt(7) = -p.kon * p.vbp * y(7) + p.koff * p.vbp * y(2);
+end
--- hw/hw1/script.m
+++ hw/hw1/script.m
@@ -22,6 +22,7 @@ p.vb   = 45 * 0.15;
 
 P0 = 100;
 
+%% Q2e
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%      SIMULATION 1      %%%%
 
@@ -31,37 +32,39 @@ step = 1/60;
 tmax = 96;
 
 sim1 = Model(p, y0);
-[t, y1, b1] = sim1.get_solution(0:step:tmax);
+[t, y1, b1] = sim1.get_solution(0:step:tmax, 0);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%      SIMULATION 2      %%%%
 
-D0 = 9.6
+D0 = 9.6;
 y0 = [D0; 0; 0; 0; 0; 0; P0];
 step = 1/60;
 tmax = 96;
 
 sim2 = Model(p, y0);
-[t, y2, b2] = sim2.get_solution(0:step:tmax);
+[t, y2, b2] = sim2.get_solution(0:step:tmax, 0);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%      SIMULATION 3      %%%%
 
-D0 = 96
+D0 = 96;
 y0 = [D0; 0; 0; 0; 0; 0; P0];
 step = 1/60;
 tmax=96;
 
 sim3 = Model(p, y0);
-[t, y3, b3] = sim3.get_solution(0:step:tmax);
+[t, y3, b3] = sim3.get_solution(0:step:tmax, 0);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%       Plotting          %%%
-% fig1 = figure;
+
 fig = tiledlayout(3, 2);
 series_name = ["0.96 nmol", "9.6 nmol", "96 nmol"];
-% ax1 = subplot(3, 2, 1);
-nexttile
+xlabel(fig, "time (hrs)");
+ylabel(fig, "Concentration [nmol]")
+
+nexttile;
 plot(...       
     t, y1(:, 1), ...
     t, y2(:, 1), ...
@@ -70,10 +73,8 @@ plot(...
 );
 legend(series_name);
 title("Free Drug Concentration in Blood");
-% ylabel("Concentration [nmol]");
 
 nexttile;
-% ax2 = subplot(3, 2, 2);
 plot(...
     t, sum(y1(:, 1:2), 2), ...
     t, sum(y2(:, 1:2), 2), ...
@@ -84,7 +85,6 @@ legend(series_name);
 title("Concentration of Total Drug in Blood");
 
 nexttile;
-% ax3 = subplot(3, 2, 3);
 plot(...
     t, y1(:, 3), ...
     t, y2(:, 3), ...
@@ -93,11 +93,9 @@ plot(...
 );
 legend(series_name);
 title("Concentration of Free Drug in Large Tumor");
-% ylabel("Concentration [nmol]");
 
-% ax4 = subplot(3, 2, 4);
 nexttile
-plot(... ax4, ...
+plot(...
     t, y1(:, 4), ...
     t, y2(:, 4), ...
     t, y3(:, 4), ...
@@ -106,7 +104,6 @@ plot(... ax4, ...
 legend(series_name);
 title("Concentration of Free Drug in Small Tumor");
 
-% ax5 = subplot(3, 2, 5);
 nexttile;
 plot(...ax5, ...
     t, y1(:, 6), ...
@@ -117,8 +114,8 @@ plot(...ax5, ...
 legend(series_name);
 title("Concentration of Free Drug in Body");
 
-nexttile %ax6 = subplot(3, 2, 6);
-plot(... %ax6, ...
+nexttile
+plot(...
     t, b1, ...
     t, b2, ...
     t, b3, ...
@@ -127,5 +124,106 @@ plot(... %ax6, ...
 legend(series_name);
 title("Mass Balance");
 
+
+%% Q2f
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%      SIMULATION 1      %%%%
+
+D0 = 0.96;
+y0 = [D0, 0, 0, 0, 0, 0, P0]';
+step = 1/60;
+tmax = 96;
+
+sim1 = Model(p, y0);
+[t, y1, b1] = sim1.get_solution(0:step:tmax, 1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%      SIMULATION 2      %%%%
+
+D0 = 9.6;
+y0 = [D0; 0; 0; 0; 0; 0; P0];
+step = 1/60;
+tmax = 96;
+
+sim2 = Model(p, y0);
+[t, y2, b2] = sim2.get_solution(0:step:tmax, 1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%      SIMULATION 3      %%%%
+
+D0 = 96;
+y0 = [D0; 0; 0; 0; 0; 0; P0];
+step = 1/60;
+tmax=96;
+
+sim3 = Model(p, y0);
+[t, y3, b3] = sim3.get_solution(0:step:tmax, 1);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%       Plotting          %%%
+
+fig = tiledlayout(3, 2);
+series_name = ["0.96 nmol", "9.6 nmol", "96 nmol"];
 xlabel(fig, "time (hrs)");
 ylabel(fig, "Concentration [nmol]")
+
+nexttile;
+plot(...       
+    t, y1(:, 1), ...
+    t, y2(:, 1), ...
+    t, y3(:, 1), ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Free Drug Concentration in Blood");
+
+nexttile;
+plot(...
+    t, sum(y1(:, 1:2), 2), ...
+    t, sum(y2(:, 1:2), 2), ...
+    t, sum(y3(:, 1:2), 2), ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Concentration of Total Drug in Blood");
+
+nexttile;
+plot(...
+    t, y1(:, 3), ...
+    t, y2(:, 3), ...
+    t, y3(:, 3), ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Concentration of Free Drug in Large Tumor");
+
+nexttile
+plot(...
+    t, y1(:, 4), ...
+    t, y2(:, 4), ...
+    t, y3(:, 4), ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Concentration of Free Drug in Small Tumor");
+
+nexttile;
+plot(...ax5, ...
+    t, y1(:, 6), ...
+    t, y2(:, 6), ...
+    t, y3(:, 6), ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Concentration of Free Drug in Body");
+
+nexttile
+plot(...
+    t, b1, ...
+    t, b2, ...
+    t, b3, ...
+    'LineWidth', 3 ...
+);
+legend(series_name);
+title("Mass Balance");
