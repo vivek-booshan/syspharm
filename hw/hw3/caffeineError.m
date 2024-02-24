ce54a2cfb0e5d003f17cf8eba6ed9c760427c56c
Vivek Booshan <vivek.booshan@gmail.com>
2024-02-24 10:51:31 -0500

    remove redundant error initialization

--- hw/hw3/caffeineError.m
+++ hw/hw3/caffeineError.m
@@ -9,13 +9,12 @@ function error = caffeineError(t_measured, y_measured, y0, p, restrict, initial_
         is10am logical = 0
     end
     p = p.*(1 - restrict) + initial_guess_params.*restrict;
-    error = 0;
     if is10am
         [~, y] = ode45(@(t, y) CaffeineODE(t, y, p), 0:1/4:1, y0);
         y0 = y(end, :);
         y0(end) = y0(end) + 310;
     end
-    [t, y] = ode45(@(t, y) CaffeineODE(t, y, p), 0:1/4:14, y0);
+    [t, y] = ode45(@(t, y) CaffeineODE(t, y, p), t_measured, y0);
     y_interp = interp1(t, y(:, 1), t_measured); % optimize for degr
-    error = error + sum((y_interp(:) - y_measured(:)).^2);
+    error = sum((y_interp(:) - y_measured(:)).^2);
 end
