4b17422a54ea8101f70e1b07f39f3beb61ec53a4
Vivek Booshan <vivek.booshan@gmail.com>
2024-02-25 20:03:51 -0500

    rewrite q1g_plotting to get neat graphs with legend

--- hw/hw3/q1g_plotting.R
+++ hw/hw3/q1g_plotting.R
@@ -1,25 +1,37 @@
-#library(ggplot2)
+setwd(Sys.getenv("PWD"))
+library(ggplot2)
+library(scales)
 
+hex = hue_pal()(5) #needed to match point with line
 questions = c('a', 'b', 'c', 'e', 'f')
-
 for (j in 1:5) {
-  p <- ggplot() + labs(title=paste0('1', questions[j]))
+  
+  data = c() #init data frame
+  p = ggplot() + 
+    labs(title=paste0('Gut Concentration using q1', questions[j])) + 
+    theme_bw() +
+    scale_x_continuous(name = 'Time (hrs)', breaks=(seq(0, 14, 2))) +
+    scale_y_continuous(name = 'Gut Concentration (mg/L)', breaks=(seq(0, 12, 1)))
+  
   for (i in 1:5) {
-    simulation_data <- read.csv(
-      paste0("subject", i, "table", questions[j], ".txt"), 
-      col.names = c('t', 'y'),
+    subject_data = read.csv(
+      paste0('subject', i, 'table', questions[j], '.txt'),
+      col.names=c('t', 'y'),
       header=FALSE
     )
+    subject = rep(paste0('subject', i), times=nrow(subject_data))
+    subject_data = cbind(subject, subject_data)
+    
+    data = rbind(data, subject_data)
+    
     real_data <- read.csv(
       paste0("subject", i, "_experimental.txt"),
       col.names = c('t', 'y'),
       header=FALSE
     )
-    real_data$t = real_data$t + 1
-    # print(real_data)
-    p <- p + geom_point(data=real_data, aes(x=t, y=y, color=i))
-    p <- p + geom_line(data=simulation_data, aes(x=t, y=y), color=i)
-    # p <- p + scale_color_manual(labels=paste("subject", i))
+    real_data$t = real_data$t + 1 # offset by hour
+    p = p + geom_point(data=real_data, aes(x=t, y=y), color=hex[i])
   }
+  p = p + geom_line(data=data, aes(x=t, y=y, color=subject))
   print(p)
 }
