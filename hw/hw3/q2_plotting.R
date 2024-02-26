184dae3c7d8870cb23034d713cb5a9c847d621f8
Vivek Booshan <vivek.booshan@gmail.com>
2024-02-25 22:38:46 -0500

    sexify q2 plotting

--- hw/hw3/q2_plotting.R
+++ hw/hw3/q2_plotting.R
@@ -1,11 +1,41 @@
+setwd(Sys.getenv("PWD"))
+
 library(RColorBrewer)
-coul = colorRampPalette(brewer.pal(8, "PiYG"))(25)
-questions = c('a', 'b', 'c')
+library(tidyr)
+library(ggplot2)
+
+sensitivities = c('14hr AUC', '1hr AUC', 'Sum of Squares')
+colnames = rep(c(paste('subject', 1:5)))
+parameters = rep(c('D1', 'D2', 'kcl', 'Vd', 'ka'))
+
+# make row 1 as col names
+header.true <- function(df) {
+  names(df) <- as.character(unlist(df[1, ]))
+  df[-1, ]
+}
+
 for (i in 1:3) {
-  sens_mat = as.matrix(read.csv(
+  sens_mat = as.data.frame(read.csv(
     paste0("q2", questions[i], ".txt"),
-    col.names=c('1', '2', '3', '4', '5'),
     header=FALSE
   ))
-  heatmap(sens_mat, Colv=NA, Rowv=NA, col=coul)
+  sens_mat = rbind(colnames, sens_mat)
+  sens_mat = header.true(sens_mat)
+  sens_mat = cbind(parameters, sens_mat)
+  sens_mat = pivot_longer(
+    sens_mat, 
+    !parameters, 
+    names_to='subject', 
+    values_to='sens'
+  )
+  sens_mat$parameters = factor(sens_mat$parameters, levels=rev(parameters))
+  print(sens_mat)
+  sens_mat$sens = as.numeric(sens_mat$sens)
+  p = ggplot(data=sens_mat, aes(x=subject, y=parameters, fill=sens)) + 
+    labs(title=paste('Local Sensitivity for', sensitivities[i])) +
+    theme_minimal() + geom_tile() +
+    scale_fill_distiller(name='Sensitivity', palette='Spectral')
+  print(p)
+
 }
+
