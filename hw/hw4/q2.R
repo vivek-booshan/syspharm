3720dd1a8d5523fa9417c313da9216800901904e
Vivek Booshan <vivek.booshan@gmail.com>
2024-03-14 23:44:36 -0400

    add labels and units

--- hw/hw4/q2.R
+++ hw/hw4/q2.R
@@ -1,11 +1,11 @@
 #### DEPENDENCIES
 setwd(Sys.getenv("PWD"))
 library(ggplot2)
-if (requireNamespace("tidyr")) {
-    pivot_longer <- tidyr::pivot_longer
-} else {
+if (!requireNamespace("tidyr")) {
     stop("tidyr not installed")
 }
+pivot_longer <- tidyr::pivot_longer
+
 
 #### PREPARE DATA
 TapData = read.csv(
@@ -31,40 +31,44 @@ TapData$simka = sim_param[, 3]
 TapData$simkCL = TapData$simCL / TapData$simV
 
 headers = c('wt', 'V', 'CL', 'ka', 'kCL', 'simV', 'simCL', 'simka', 'simkCL')
-
+units = c('kg', 'L', 'L/hr', 'L/hr', '1/hr')
 #### PLOTS
+#### q2a
 for (i in 2:5) {
     p = ggplot() + 
         geom_point(data=TapData, aes(x=wt, y=!!sym(headers[i]), color='measured')) +
         geom_point(data=TapData, aes(x=wt, y=!!sym(headers[i + 4]), color='simulated')) +
-        labs(y=headers[i]) +
+        labs(y=paste0(headers[i], " (", units[i], ")"), x = "weight (kgs)") +
+        labs(title=paste("Overlay of Allometric", headers[i], "with Real", headers[i])) +
         theme_bw()
     ggsave(paste0("q2a_", headers[i], ".png"), bg='white')
     print(p)
 }
 
+#### q2b
 longTapData = pivot_longer(TapData, cols=-wt, names_to="parameter", values_to="value")
 longTapData$parameter = as.factor(longTapData$parameter)
 p = ggplot(data=longTapData) + 
     geom_violin(aes(x=parameter, y=value),trim=FALSE) +
     scale_y_log10() +
+    labs(title="Distribution of Parameter Values") +
+    labs(x="Measured and Predicted Parameters", y="Arbitrary Log10 Units") +
     theme_bw()
 ggsave("q2b_violin.png", bg='white')
 print(p)
 
+#### q2c
 for (i in 2:5) {
     p = ggplot() + 
         geom_point(
             data=TapData, 
-            aes(x=!!sym(headers[i]), y=!!sym(headers[i+4]), 
-                color='pink')) +
-        labs(
-            title=paste(
+            aes(x=!!sym(headers[i]), y=!!sym(headers[i+4]))) +
+        labs(title=paste(
                 "Comparison between", headers[i], 
-                "with simulated", headers[i+4]), 
-            x=paste("IIV", headers[i]),
-            y=paste("non-IIV", headers[i])
-        )
+                "with Allometric", headers[i]), 
+            x=paste0("Real ", headers[i], "(", units[i], ")"), 
+            y=paste0("Allometric ", headers[i], "(", units[i], ")")) +
+        theme_bw()
         ggsave(paste0("q2c_", headers[i], ".png"), bg='white')
     print(p)
 }
