4a5f741671bd6ea72c955b28e78d701ff46e1f4c
Vivek Booshan <vivek.booshan@gmail.com>
2024-04-20 17:26:54 -0400

    remove unnecessary files

--- final_project/pk_eqns.m
+++
@@ -1,9 +0,0 @@
-function dydt = pk_eqns(t,y,p)
-
-dydt = zeros(4,1);    
- dydt(1) =     p.F*p.ka*y(3) - p.kCL*y(1) - p.k12*y(1)+p.k21*y(2) ; 
- dydt(2) =     p.k12*y(1)-p.k21*y(2) ;
- dydt(3) =        -p.ka*y(3)*p.Vc ; 
- dydt(4) = (1-p.F)*p.ka*y(3)     + p.kCL*y(1)*p.Vc ; 
- 
- 
