a895468319db80f2a57ebaa20707dd61f39e9cfb
Vivek Booshan <vivek.booshan@gmail.com>
2024-04-21 13:52:24 -0400

    add pk and pd simulations with distinct parameter functions; decrease resolution of simulations and use ode23s

--- final_project/Patients.m
+++ final_project/Patients.m
@@ -56,37 +56,95 @@ classdef Patients < handle
     end
 
     methods (Static)
-        function p = parameters(Vc, Vp, ka, CL, Q)
-            p = struct('Vc', Vc, 'Vp', Vp, 'ka', ka, 'CL', CL, 'Q', Q);
+        function p = pkParameters(Vc, Vp, ka, CL, Q, F)
+            arguments
+                Vc single % Central Volume Distribution (L)
+                Vp single % Peripheral Volume Distribution (L)
+                ka single % absorbance rate (1/hr)
+                CL single % Clearance (L/hr)
+                Q single % Intercompartmental Clearance (L/hr)
+                F single = 0.8 % Bioavailability
+            end
+            p = struct( ...
+                'Vc', Vc, 'Vp', Vp, 'ka', ka, 'CL', CL, 'Q', Q, 'F', F, ...
+                'kCL', CL / Vc, 'k12', Q/Vc, 'k21', Q/Vp ...
+            );
+        end
+        function p = pdParameters(Vc, Vp, ka, CL, Q, F, kDIS, kOFF, kOUT, E0H, E0G, EC50, PLAC, Hlim, FPG)
+            arguments
+                Vc single % Central Volume Distribution (L)
+                Vp single % Peripheral Volume Distribution (L)
+                ka single % absorbance rate (1/hr)
+                CL single % Clearance (L/hr)
+                Q single % Intercompartmental Clearance (L/hr)
+                F single = 0.8 % Bioavailability
+                kDIS single = 0.0215 % FG disease progression rate (mM/wk)
+                kOFF single = 0.00306 % Offset rate (1/hr)
+                kOUT single = 0.00156 % Turnover rate const for HbA1c (1/hr)
+                E0H single = 8.25 % Baseline HbA1c (%)
+                E0G single = 9.23 % Baseline FG (mmol/L)
+                EC50 single = 144 % [Tirzepatide] with 50% max effect (ng/mL)
+                PLAC single = 0.0846 % Placebo fractional reduction of FG
+                Hlim single = 5.02 % Limit for HbA1c-Emax (%)
+                FPG single = 0.799 % Expo for effect of FG on HbA1c (gamma)
+            end
+            p = struct( ...
+                'Vc', Vc, 'Vp', Vp, 'ka', ka, 'CL', CL, 'Q', Q, 'F', F, ...
+                'kCL', CL/Vc, 'k12', Q/Vc, 'k21', Q/Vp, ...
+                'kDIS', kDIS, 'kOFF', kOFF, 'kOUT', kOUT, 'E0H', E0H, ...
+                'E0G', E0G, 'EC50', EC50, 'PLAC', PLAC, 'Hlim', Hlim, 'FPG', FPG ...
+            );
         end
         
-        function [time, solution] = simulateRegime(p, y0, dose)
-            p.kCL = p.CL / p.Vc;
-            p.k12 = p.Q / p.Vc;
-            p.k21 = p.Q / p.Vp;
-            p.F = 0.8;
+        function [time, solution] = simulatePK(p, y0, dose)
+            dose_count = length(dose);
+            resolution = 1;
+            sim_len = resolution*168 + 1;
+            time = zeros(dose_count*sim_len, 1); % 16 simulations of 10081 time steps
+            solution = zeros(dose_count*sim_len, 4);
+            for i = 1:dose_count
+                y0(3) = y0(3) + dose(i);
+                [t, y] = ode23s(@Patients.tirzepatidePK, 168*(i-1):(1/resolution):168*i, y0, [], p);
+                y0 = y(end, :);
+                range = sim_len*(i-1)+1:sim_len*i;
+                time(range) = t;
+                solution(range, :) = y;
+            end
+        end
 
+        function [time, solution] = simulatePD(p, y0, dose)
             options = odeset('MaxStep', 5e-2, 'AbsTol', 1e-5, 'RelTol', 1e-5, 'InitialStep', 1e-2);
             time = zeros(16*10081, 1); % 16 simulations of 10081 time steps
-            solution = zeros(16*10081, 4);
+            solution = zeros(16*10081, 7);
             for i = 1:16
                 y0(3) = y0(3) + dose(i);
-                [t, y] = ode45(@Patients.tirzepatideODE, 168*(i-1):(1/60):168*i, y0, options, p);
+                [t, y] = ode45(@Patients.tirzepatidePD, 168*(i-1):(1/60):168*i, y0, options, p);
                 y0 = y(end, :);
                 range = 10081*(i-1)+1:10081*i;
                 time(range) = t;
                 solution(range, :) = y;
             end
-        end    
+        end
     end
 
     methods (Access = private, Static)
-        function dydt = tirzepatideODE(t, y, p)
+        function dydt = tirzepatidePK(t, y, p)
             dydt = zeros(4, 1);
             dydt(1) = p.F * p.ka * y(3) - p.kCL * y(1) - p.k12 * y(1) + p.k21 * y(2);
             dydt(2) = p.k12 * y(1) - p.k21 * y(2);
             dydt(3) = -p.ka * y(3) * p.Vc;
             dydt(4) = (1 - p.F) * p.ka * y(3) + p.kCL * y(1) * p.Vc;
+        end
+
+        function dydt = tirzepatidePD(t, y, p)
+            dydt = zeros(7, 1);
+            dydt(1) = p.F*p.ka*y(3) - p.kCL*y(1) - p.k12*y(1) + p.k21*y(2); 
+            dydt(2) = p.k12*y(1) - p.k21*y(2);
+            dydt(3) = -p.ka*y(3)*p.Vc; 
+            dydt(4) = (1-p.F)*p.ka*y(3) + p.kCL*y(1)*p.Vc;
+            dydt(5) = p.kDIS;
+            dydt(6) = p.kOFF*((1-p.PLAC-(p.Hlim/p.E0H)^(1/p.FPG))*y(1)/(y(1)+p.EC50)-y(6));
+            dydt(7) = (p.kOUT*p.E0H*(y(5)-y(5)*y(6))^p.FPG/(p.E0G^p.FPG))-p.kOUT*y(7);
         end
     end
 end
--- final_project/pk_script.m
+++ final_project/pk_script.m
@@ -6,17 +6,24 @@ Vc = [2.07, 2.48, 2.98];
 Vp = [3.48, 3.91, 4.17];
 
 % ICS
-y0 = [0, 0, 0, 0];
+y0 = zeros(1, 4);
 % weekly dosing regime
 dose = [2.5, 2.5, 2.5, 2.5, 5, 5, 5, 5, 7.5, 7.5, 7.5, 7.5, 10, 10, 10, 10];
 
-[A, B, C, D, E] = ndgrid(Vp, Vc, ka, CL, Q);
+[A, B, C, D, E] = ndgrid(Vc, Vp, ka, CL, Q);
 max = numel(A);
 
 gcp();
 parfor i = 1:max
     a = A(i); b = B(i); c = C(i); d = D(i); e = E(i);
-    p = Patients.parameters(a, b, c, d, e);
-    [t, y] = Patients.simulateRegime(p, y0, dose);
+    p = Patients.pkParameters(a, b, c, d, e);
+    [t, y] = Patients.simulatePK(p, y0, dose);
     writematrix([t, y], sprintf('output_%d_%d_%d_%d_%d.csv', a, b, c, d, e));
 end 
+
+%%
+% p = Patients.pdParameters(Vc(1), Vp(1), ka(1), CL(1), Q(1));
+% y0 = zeros(1, 7);
+% tic
+% [t5, y5] = Patients.simulatePD(p, y0, dose);
+% toc
