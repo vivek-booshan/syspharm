b80c98b41bb46ddba5826a017aaac48b5d16044b
Vivek Booshan <vivek.booshan@gmail.com>
2024-04-23 19:31:16 -0400

    add better labels and add median data line

--- final_project/server.R
+++ final_project/server.R
@@ -3,7 +3,12 @@ library(shiny)
 library(ggplot2)
 library(plotly)
 
+FILE = paste0("data/",
+            "output_2.480000e+00_3.910000e+00_3.630000e-02_3.260000e-02_1.250000e-01",
+            ".csv")
+median_data = read.csv(FILE, header=FALSE, col.names=c("t", "y1", "y2", "y3", "y4"))
 function (input, output) {
+    
     data <- reactive({
         FILE = paste0('data/output_', input$Vc, '_', input$Vp, '_', input$ka, '_', input$CL, '_', input$Q, '.csv')
         read.csv(FILE, header=FALSE, col.names=c("t", "y1", "y2", "y3", "y4"))
@@ -13,20 +18,28 @@ function (input, output) {
     })
     output$plot1 <- renderPlotly({
         p <- p() + 
+            geom_line(data=median_data, aes(x=t, y=y1), color='pink') +
             geom_line(aes(x=t, y=y1)) +
-            labs(x="Time (Hrs)", y="Central Compartment (mg/0.5ml)", title="Tirzepatide Central Compartment Concentration Over 12 Weeks")
-        print(p)
+            labs(x="Time (Hrs)", y="Central Compartment (mg/0.5ml)", 
+                 title="Tirzepatide Central Compartment Concentration Over 16 Weeks") +
+            scale_x_continuous(breaks=seq(0, 16, by=4))
     })
     
     output$plot2 <- renderPlotly({
         p <- p() + 
+            geom_line(data=median_data, aes(x=t, y=y2), color='pink') +
             geom_line(aes(x=t, y=y2)) + 
-            labs(x="Time (Hrs)", y="Peripheral Compartment (mg/0.5ml)", title="Tirzepatide Peripheral Compartment Concentration Over 12 Weeks")
+            labs(x="Time (Hrs)", y="Peripheral Compartment (mg/0.5ml)", 
+                 title="Tirzepatide Peripheral Compartment Concentration Over 16 Weeks") +
+            scale_x_continuous(breaks=seq(0, 16, by=4))
     })
     
     output$plot3 <- renderPlotly({
         p <- p() + 
+            geom_line(data=median_data, aes(x=t, y=y4), color='pink') +
             geom_line(aes(x=t, y=y4)) + 
-            labs(x="Time (Hrs)", y="Cumulative Dose Concentration (mg/0.5ml)", title="Cumulative Dose Concentration Over 12 Weeks")
+            labs(x="Time (Hrs)", y="Cumulative Dose Concentration (mg/0.5ml)", 
+                 title="Cumulative Dose Concentration Over 16 Weeks") +
+            scale_x_continuous(breaks=seq(0, 16, by=4))
     })
 }
